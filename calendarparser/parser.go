5c0bcfa1b4b7f38bcf0b36243933c4e9a76eada2
medze <medzernik1@gmai.com>
2021-08-24 12:53:59 +0200

    Began work on the cal parser

--- calendarparser/parser.go
+++ calendarparser/parser.go
@@ -1,5 +1,43 @@
 package calendarparser
 
-func initialization() {
+import (
+	"fmt"
+	"github.com/apognu/gocal"
+	"os"
+	"time"
+)
+
+type CalendarInformation struct {
+	EventName string
+	DateStart time.Time
+	DateEnd   time.Time
+}
+
+func Initialization() {
+	f, _ := os.Open("calendar_files/01cal.ics")
+	defer f.Close()
+
+	start, end := time.Now(), time.Now().Add(12*30*24*time.Hour)
+
+	c := gocal.NewParser(f)
+	c.Start, c.End = &start, &end
+	c.Parse()
+
+	//2D slice array of [X][Y]. X is the room, Y is the subjects within it.
+	var CalendarEvents [][]CalendarInformation
+
+	ParseIntoVariables(c, &CalendarEvents)
+
+}
+
+func ParseIntoVariables(c *gocal.Gocal, calendarEvents *[][]CalendarInformation) {
+	//var i int
+	//var j int
+
+	//Search through the entire file
+	for _, e := range c.Events {
+		fmt.Println(e.Summary)
+		//if e.
+	}
 
 }
--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"FHITabule/calendarparser"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
+	go calendarparser.Initialization()
+
 	a := app.New()
 	w := a.NewWindow("Hello")
 
