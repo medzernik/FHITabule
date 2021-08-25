c3a7ddda862ba6e6d93dcc3e63c1b3a620ab7fb9
medze <medzernik1@gmai.com>
2021-08-25 18:58:13 +0200

    Finishing the parser work

--- calendarparser/parser.go
+++ calendarparser/parser.go
@@ -18,12 +18,14 @@ type CalendarInformation struct {
 }
 
 func Initialization() {
+	var timeToDisplayHours time.Duration = 6
 
 	//TODO: make this load more files and also into an array
 	f, _ := os.Open("calendar_files/01cal.ics")
 	defer f.Close()
 
-	start, end := time.Now(), time.Now().Add(12*30*24*time.Hour)
+	//*10000 is a debug
+	start, end := time.Date(2021, 10, 11, 10, 0, 0, 0, time.Now().Location()), time.Date(2021, 10, 11, 10, 0, 0, 0, time.Now().Location()).Add(timeToDisplayHours*time.Hour)
 
 	c := gocal.NewParser(f)
 	c.Start, c.End = &start, &end
@@ -34,7 +36,7 @@ func Initialization() {
 }
 
 func ParseIntoVariables(c *gocal.Gocal) {
-	//var todaysDate time.Time = time.Now()
+	//var todaysDate time.Time = time.Date(2021, 9, 28, time.Now().Hour(), time.Now().Minute(), time.Now().Second(), time.Now().Nanosecond(), time.Now().Location())
 	//2D slice array of [X][Y]. X is the room, Y is the subjects within it.
 
 	var location []string
@@ -46,55 +48,65 @@ func ParseIntoVariables(c *gocal.Gocal) {
 	var summaryProcessed []string
 	var summaryTemp []string
 	var CalendarEvents []CalendarInformation = make([]CalendarInformation, len(c.Events))
+	var iterator int = 0
+
 	//Search through the entire file
 	//TODO: make it an array
 
 	for _, e := range c.Events {
-		for i := 0; i < len(c.Events); i++ {
+		fmt.Printf("%s on %s - %s\n", e.Summary, e.Start, e.End)
 
-			//Parsing the location and getting a room number and floor number
-			location = strings.Split(e.Location, ", ")
-			locationProcessed = strings.Split(location[0], ".")
-			floor = locationProcessed[0]
-			room = locationProcessed[1]
+		//Parsing the location and getting a room number and floor number
+		location = strings.Split(e.Location, ", ")
+		locationProcessed = strings.Split(location[0], ".")
+		floor = locationProcessed[0]
+		room = locationProcessed[1]
 
-			//Parsing the ridiculous and messy Summary line. First we get the information about whether it's a course or presentation
-			if strings.Contains(e.Summary, " C ") {
-				summary = strings.Split(e.Summary, " C ")
-				presentationBool = false
-			} else if strings.Contains(e.Summary, " P ") {
-				summary = strings.Split(e.Summary, " P ")
-				presentationBool = true
-			}
-
-			//Then after splitting the line in a slice we further split the slice by every space
-			summaryTemp = strings.SplitN(summary[0], " ", -1)
-
-			//We know that the name of the subject can be multiple words but it always starts after the first--
-			//--2 space members. We get that until the end (end was split by C or P, since its always before that)
-			summaryProcessed = summaryTemp[2:]
-
-			//Important debug statements
-			/*
-				fmt.Println("-----\nNESPRACOVANE: ", summary)
-				fmt.Println("SPRACOVANE: ", summaryProcessed, "\n------")
-				fmt.Println(presentationBool)
-
-				fmt.Println("\nTMP:")
-				fmt.Println(summary)
-
-			*/
-
-			CalendarEvents[i].EventName = strings.Join(summaryProcessed[0:], " ")
-			CalendarEvents[i].DateStart = *e.Start
-			CalendarEvents[i].DateEnd = *e.End
-			CalendarEvents[i].Floor = floor
-			CalendarEvents[i].Room = room
-			CalendarEvents[i].Presentation = presentationBool
-
+		//Parsing the ridiculous and messy Summary line. First we get the information about whether it's a course or presentation
+		if strings.Contains(e.Summary, " C ") {
+			summary = strings.Split(e.Summary, " C ")
+			presentationBool = false
+		} else if strings.Contains(e.Summary, " P ") {
+			summary = strings.Split(e.Summary, " P ")
+			presentationBool = true
 		}
-		fmt.Printf("%+v\n", CalendarEvents)
-		fmt.Println(len(CalendarEvents))
+
+		//Then after splitting the line in a slice we further split the slice by every space
+		summaryTemp = strings.SplitN(summary[0], " ", -1)
+
+		//We know that the name of the subject can be multiple words but it always starts after the first--
+		//--2 space members. We get that until the end (end was split by C or P, since its always before that)
+		summaryProcessed = summaryTemp[2:]
+
+		//Important debug statements
+		/*
+			fmt.Println("-----\nNESPRACOVANE: ", summary)
+			fmt.Println("SPRACOVANE: ", summaryProcessed, "\n------")
+			fmt.Println(presentationBool)
+
+			fmt.Println("\nTMP:")
+			fmt.Println(summary)
+
+		*/
+
+		//fmt.Printf("%+v\n", e)
+
+		CalendarEvents[iterator].EventName = strings.Join(summaryProcessed[0:], " ")
+		CalendarEvents[iterator].DateStart = *e.Start
+		CalendarEvents[iterator].DateEnd = *e.End
+		CalendarEvents[iterator].Floor = floor
+		CalendarEvents[iterator].Room = room
+		CalendarEvents[iterator].Presentation = presentationBool
+
+		iterator += 1
+
+	}
+
+	//fmt.Printf("%+v\n", CalendarEvents)
+	//fmt.Println(len(CalendarEvents))
+	for l := range CalendarEvents {
+		fmt.Printf("%+v\n", CalendarEvents[l])
+
 	}
 
 }
