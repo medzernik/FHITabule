c4495b12dcf6c58a62b96947d440bdf16ef46473
medze <medzernik1@gmai.com>
2021-08-31 19:20:32 +0200

    First working version (sort of)

--- main.go
+++ main.go
@@ -8,54 +8,64 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 	"time"
 )
 
-func main() {
-	cmd := exec.Command("node app.js")
-
-	errJS := cmd.Run()
-
-	if errJS != nil {
-		fmt.Println(errJS)
+func runLocalWebpageDisplay() {
+	command := "/home/medzernik/.nvm/versions/node/v16.8.0/bin/node app.js"
+	parts := strings.Fields(command)
+	_, err := exec.Command(parts[0], parts[1:]...).Output()
+	if err != nil {
+		panic(err)
 	}
+}
+
+func main() {
+
+	go runLocalWebpageDisplay()
 
 	calendarparser.Initialization()
 
 	var runElectron = true
 
 	if runElectron == true {
+
 		// Make HTTP GET request
 		//response, err := http.Get("https://imhd.sk/ba/online-zastavkova-tabula?theme=white&zoom=67&st=66")
 		var a, _ = astilectron.New(log.New(os.Stderr, "", 0), astilectron.Options{
-			AppName:            "<your app name>",
+			AppName:            "DAVID MANCA - EUBA TABULE",
 			AppIconDefaultPath: "", // If path is relative, it must be relative to the data directory
 			AppIconDarwinPath:  "", // Same here
 			VersionAstilectron: "",
 			VersionElectron:    "",
+			SingleInstance:     true,
 		})
 		defer a.Close()
 
 		// Start astilectron
 		a.Start()
 		var w, _ = a.NewWindow("https://imhd.sk/ba/online-zastavkova-tabula?theme=white&zoom=67&st=66", &astilectron.WindowOptions{
-			Center:      astikit.BoolPtr(true),
-			Height:      astikit.IntPtr(600),
-			Width:       astikit.IntPtr(600),
-			Minimizable: astikit.BoolPtr(false),
-			Fullscreen:  astikit.BoolPtr(true),
+			Center:          astikit.BoolPtr(true),
+			Height:          astikit.IntPtr(600),
+			Width:           astikit.IntPtr(600),
+			Minimizable:     astikit.BoolPtr(false),
+			Fullscreen:      astikit.BoolPtr(true),
+			BackgroundColor: astikit.StrPtr("black"),
 		})
 		w.Create()
 
 		//TODO: google API cesta na hopu MHD + auto
-		var link_imhd string = "https://imhd.sk/ba/online-zastavkova-tabula?theme=white&zoom=67&st=66"
+		links := [2]string{"https://imhd.sk/ba/online-zastavkova-tabula?theme=white&zoom=67&st=66", "http://localhost:3000"}
 
 		for {
-			err := w.ExecuteJavaScript("window.location.href = " + link_imhd + ";")
-			if err != nil {
-				fmt.Println("ERROR:", err)
+			for i := range links {
+				err := w.ExecuteJavaScript("window.location.href = \"" + links[i] + "\";")
+				if err != nil {
+					fmt.Println("ERROR:", err)
+				}
+				time.Sleep(5 * time.Second)
 			}
-			time.Sleep(10 * time.Second)
 		}
 
 		// Blocking pattern
